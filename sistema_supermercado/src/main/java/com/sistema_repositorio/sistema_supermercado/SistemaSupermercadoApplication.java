2e33ab69ebbe862546ed22b023a6f94eb71a06e5
Marcos Vinicius <viniciusferreirag213@gmail.com>
2023-03-29 20:41:43 -0300

    initial commit

---
+++ sistema_supermercado/src/main/java/com/sistema_repositorio/sistema_supermercado/SistemaSupermercadoApplication.java
@@ -0,0 +1,13 @@
+package com.sistema_repositorio.sistema_supermercado;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaSupermercadoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaSupermercadoApplication.class, args);
+	}
+
+}
