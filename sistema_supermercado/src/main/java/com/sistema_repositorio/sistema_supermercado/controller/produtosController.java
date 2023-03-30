45b9db7ccc427e506e2e12275ee1d263d793f5f0
PauloD-Souza <86973749+PauloD-Souza@users.noreply.github.com>
2023-03-30 15:22:06 -0300

    Codigos

--- sistema_supermercado/src/main/java/com/sistema_repositorio/sistema_supermercado/controller/produtosController.java
+++ sistema_supermercado/src/main/java/com/sistema_repositorio/sistema_supermercado/controller/produtosController.java
@@ -1,12 +1,36 @@
 package com.sistema_repositorio.sistema_supermercado.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
+import com.sistema_repositorio.sistema_supermercado.model.produto;
+import com.sistema_repositorio.sistema_supermercado.repository.produtoRepository;
+import com.sistema_repositorio.sistema_supermercado.sequenceMongodb.sequenceGenerator;
+import jakarta.validation.Valid;
 
 @RestController
 @RequestMapping("/produtos")
 @Validated
 public class produtosController {
+    
+    @Autowired
+    private produtoRepository produtoRepository;
+
+    @GetMapping
+    public ResponseEntity<Object> listarClientes() {
+        return ResponseEntity.status(HttpStatus.OK).body(this.produtoRepository.findAll());
+    }
+    @PostMapping
+    public ResponseEntity<Object> adicionarProduto(@Valid @RequestBody produto p) {
+        p.setId(sequenceGenerator.generateSequence(produto.SEQUENCE_NAME));
+        return ResponseEntity.status(HttpStatus.CREATED).body(this.produtoRepository.save(p));
+    }
     
 }
--- sistema_supermercado/src/main/java/com/sistema_repositorio/sistema_supermercado/model/produto.java
+++ sistema_supermercado/src/main/java/com/sistema_repositorio/sistema_supermercado/model/produto.java
@@ -1,13 +1,17 @@
 package com.sistema_repositorio.sistema_supermercado.model;
 import java.math.BigDecimal;
 
-import org.hibernate.validator.constraints.br.CPF;
 import org.springframework.data.annotation.Id;
-import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
 import lombok.Data;
+
+import org.springframework.data.annotation.Transient;
+@Data
 public class produto {
+
+    @Transient
+    public static final String SEQUENCE_NAME = "products_sequence";
     
     @Id
     private long id;
