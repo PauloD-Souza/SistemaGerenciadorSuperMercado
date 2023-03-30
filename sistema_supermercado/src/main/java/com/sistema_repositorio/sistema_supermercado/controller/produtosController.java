a8e6b898ab15e33582371112f32bcb9fb3db8869
PauloD-Souza <86973749+PauloD-Souza@users.noreply.github.com>
2023-03-30 19:16:32 -0300

    Codigos

--- sistema_supermercado/src/main/java/com/sistema_repositorio/sistema_supermercado/controller/produtosController.java
+++ sistema_supermercado/src/main/java/com/sistema_repositorio/sistema_supermercado/controller/produtosController.java
@@ -4,9 +4,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import java.util.Optional;
+
 
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -31,6 +35,17 @@ public class produtosController {
     public ResponseEntity<Object> adicionarProduto(@Valid @RequestBody produto p) {
         p.setId(sequenceGenerator.generateSequence(produto.SEQUENCE_NAME));
         return ResponseEntity.status(HttpStatus.CREATED).body(this.produtoRepository.save(p));
+    }
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Object> deletarProduto(@PathVariable long id) {
+        Optional<produto> p = this.produtoRepository.findById(id);
+
+        if (p.isPresent()) {
+            this.produtoRepository.delete(p.get());
+            return ResponseEntity.status(HttpStatus.OK).body("Produto" + p.get().getNome() +""+ " excluido com sucesso.");
+        }
+
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Produto não encontrado na base de dados");
     }
     
 }
