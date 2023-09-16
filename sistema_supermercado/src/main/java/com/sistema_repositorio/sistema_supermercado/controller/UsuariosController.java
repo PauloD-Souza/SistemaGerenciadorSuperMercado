0d51753b54c8a4700dde7be07d39ffbb7e1a53c6
Paulo Souza <paulo.souza@dcx.ufpb.br>
2023-09-16 14:39:06 -0300

    Implementação parcial do controller do users

--- sistema_supermercado/src/main/java/com/sistema_repositorio/sistema_supermercado/controller/UsuariosController.java
+++ sistema_supermercado/src/main/java/com/sistema_repositorio/sistema_supermercado/controller/UsuariosController.java
@@ -3,11 +3,13 @@ package com.sistema_repositorio.sistema_supermercado.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.sistema_repositorio.sistema_supermercado.model.ErroResponse;
 import com.sistema_repositorio.sistema_supermercado.model.Funcionarios;
 import com.sistema_repositorio.sistema_supermercado.model.Usuarios;
 import com.sistema_repositorio.sistema_supermercado.repository.usuariosRepository;
@@ -16,7 +18,7 @@ import com.sistema_repositorio.sistema_supermercado.sequenceMongodb.sequenceGene
 import jakarta.validation.Valid;
 
 @RestController
-@RequestMapping("/login")
+@RequestMapping("/teste")
 public class UsuariosController {
     
     @Autowired
@@ -29,5 +31,15 @@ public class UsuariosController {
     public ResponseEntity<Usuarios> criarUsuario(@Valid @RequestBody Usuarios usuario) {
         usuario.setId(sequenceGenerator.generateSequence(Usuarios.SEQUENCE_NAME));
         return ResponseEntity.status(HttpStatus.CREATED).body(this.usuariosRepository.save(usuario));
+    }
+    @GetMapping
+    public ResponseEntity<Object> listarFuncionarios() {
+        long count = usuariosRepository.count();
+        if (count == 0){
+            ErroResponse erroResponse = new ErroResponse("Não tem nenhum funcionário cadastrado");
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(erroResponse);
+        }else{
+           return ResponseEntity.status(HttpStatus.OK).body(this.usuariosRepository.findAll());
+        }
     }
 }
